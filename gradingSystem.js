e03b7ccd231eb69f63640855cc905678795c2de1
JAY <jitupassi437@gmail.com>
2025-07-03 19:00:39 +0530

    Update gradingSystem.js

--- gradingSystem.js
+++ gradingSystem.js
@@ -1,9 +1,11 @@
 
-let marks = 6001;
+let marks = 96;
 
 function getMyGrade(marks) {
+
     if (marks<0 || marks >100) {
         console.log("Enter 1-100 only")
+      // checks the value under 1 - 100 
         
     }
     else if (marks>=80 && marks <= 100) {
