50cf58b3df9831f9ae77ae6821854b97c81d7818
JAY <jitupassi437@gmail.com>
2025-07-07 18:41:40 +0530

    pattern practice

---
+++ 02pattern.js
@@ -0,0 +1,13 @@
+// this pattern using for loop
+// + 
+// + + 
+// + + + 
+
+for(let i =1; i<=3; i++){
+    let str= ".."
+    for(let j=1; j<=i; j++){
+        str += "+ ";
+    }
+    console.log(str)
+}
+
