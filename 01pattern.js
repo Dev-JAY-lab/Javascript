6ec16d571927f61bb79d69ec201d144d6a76ed2e
JAY <jitupassi437@gmail.com>
2025-07-07 13:56:26 +0530

    pattern

---
+++ 01pattern.js
@@ -0,0 +1,20 @@
+
+// printing this pattern using nested for loop
+// + + + 
+// + + + 
+// + + + 
+
+
+
+
+for (let i = 1; i <= 3; i++) {
+    let str = "";
+    for (let j = 1; j <= 3; j++) {
+        str += "+ ";
+    }
+    console.log(str);
+}
+
+
+
+
