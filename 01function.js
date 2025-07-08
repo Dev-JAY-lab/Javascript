1b6290039111bbbc51772bacf54b9a1996226885
JAY <jitupassi437@gmail.com>
2025-07-08 10:36:01 +0530

    functions

---
+++ 01function.js
@@ -0,0 +1,28 @@
+// const sayMyName= function(){
+//     console.log("JAY");
+// }
+// sayMyName();
+
+// let a = 200;
+// let b = 100;
+// function add (a ) {
+    
+//     // let s =12;
+//     let r= 10;
+//     for(let i =1 ;i<=r; i++){
+//         console.log(a*i);
+//     }
+
+// }
+// add(12);
+
+  // Arowfunction
+const sum = a => a * a
+console.log(sum(4));
+
+
+//  iife:
+ let name= "JAY";
+((name) =>{
+    console.log("Hay" ,  name);
+})();
---
+++ 02functionPrintTable.js
@@ -0,0 +1,9 @@
+function add (a ) {
+
+    let r= 10;
+    for(let i =1 ;i<=r; i++){
+        console.log(a*i);
+    }
+    
+}
+add(12);
