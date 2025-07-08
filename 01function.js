576085bed60003aeacfa5ec66e3e39e4af6604f8
JAY <jitupassi437@gmail.com>
2025-07-08 11:56:21 +0530

    01function.js

--- 01function.js
+++ 01function.js
@@ -1,28 +1,19 @@
-// const sayMyName= function(){
-//     console.log("JAY");
-// }
-// sayMyName();
-
-// let a = 200;
-// let b = 100;
-// function add (a ) {
-    
-//     // let s =12;
-//     let r= 10;
-//     for(let i =1 ;i<=r; i++){
-//         console.log(a*i);
-//     }
-
-// }
-// add(12);
-
-  // Arowfunction
-const sum = a => a * a
-console.log(sum(4));
+const sayMyName= function(){
+    console.log("JAY");
+}
+sayMyName();
 
 
-//  iife:
- let name= "JAY";
-((name) =>{
-    console.log("Hay" ,  name);
-})();
+
+
+// const sum = a => a * a
+// console.log(sum(4));
+
+
+
+
+
+//  let name= "JAY";
+// ((name) =>{
+//     console.log("Hay" ,  name);
+// })();
