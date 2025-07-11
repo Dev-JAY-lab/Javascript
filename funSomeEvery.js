c62447cc4acca258b44bc8432d135a84f262aaeb
JAY <jitupassi437@gmail.com>
2025-07-11 12:26:21 +0530

    methods.js

--- funSomeEvery.js
+++ funSomeEvery.js
@@ -12,3 +12,14 @@ console.log(arr1.every(function even(num) {
 
 
 }
+
+{
+let arr1 = [32,54,6,8, 5, 9, 0 ,8,4,2]
+arr1.forEach( (num)=> {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+}); // Output: 32, 54, 6, 8, 0, 8, 4, 2
+
+
+}
