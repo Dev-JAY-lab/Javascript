182e49fce755841508091f06f8e3706e8e901074
JAY <jitupassi437@gmail.com>
2025-07-18 11:29:29 +0530

    Removed unnecessary comments

--- 01function.js
+++ 01function.js
@@ -4,15 +4,6 @@ const sayMyName= function(){
 sayMyName();
 
 
-
-
-// const sum = a => a * a
-// console.log(sum(4));
-
-
-
-
-
 //  let name= "JAY";
 // ((name) =>{
 //     console.log("Hay" ,  name);
