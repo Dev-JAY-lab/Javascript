fa920d68258e26af3fadcf7daefc35e3fb709c52
Dev-JAY-lab <jitupassi437@gmail.com>
2025-08-11 00:19:05 -0700

    commentsAdded

--- foreach.js
+++ foreach.js
@@ -1,5 +1,5 @@
 // this code shows how  you can sort value using sort method and forof loop  
-let a=[ 33, 22, 55, 45, 1 ,4 , 8, 1,2,43 ,,5,5,]
+let a=[ 33, 22, 5544, 45, 1 ,4 , 8, 1,2,43 ,,5,5,]
 let sortd= a.sort((a,b)=>{
     return a-b
 })
